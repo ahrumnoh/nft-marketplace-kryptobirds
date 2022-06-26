47222e349caa2ee668452e42ded3fd5b851a5c9d
Rose Noh <sydneyroseenglish@gmail.com>
2022-06-26 19:38:09 +0900

    KBMarketContract
    changes updated

--- contracts/KBMarket.sol
+++ contracts/KBMarket.sol
@@ -18,7 +18,7 @@ contract KBMarket is ReentrancyGuard {
     //array need to know the length - help to keep track for arrays
 
     Counters.Counter private _tokenIds;
-    Counters.Counter private _tokenSold;
+    Counters.Counter private _tokensSold;
 
     //determine who is the owner of the contract
     //charge a listing fee so the owner makes a commission
@@ -110,6 +110,102 @@ contract KBMarket is ReentrancyGuard {
 
         //NFT transaction
 
+        IERC721(nftContract).transferFrom(msg.sender, address(this), tokenId);
 
+
+        emit MarketTokenMinted(
+            itemId,
+            nftContract,
+            tokenId,
+            msg.sender,
+            address(0),
+            price,
+            false
+        );
     }
+
+
+        //function to conduct transactions and market sales
+
+        function createMarketSale(
+            address nftContract,
+            uint itemId)
+            public payable nonReentrant {
+                uint price = idToMarketToken[itemId].price;
+                uint tokenId = idToMarketToken[itemId].tokenId;
+                require(msg.value == price, 'Please submit the asking price in order to continue');
+
+                //transfer the amount to the seller
+                idToMarketToken[itemId].seller.transfer(msg.value);
+
+
+                //transfer the token from contract address to the buyer
+                IERC721(nftContract).transferFrom(address(this), msg.sender, tokenId);   
+                idToMarketToken[itemId].owner = payable(msg.sender);
+                idToMarketToken[itemId].sold = true;
+                _tokensSold.increment();            
+
+                payable(owner).transfer(listingPrice);
+
+            }
+
+
+        //function to fetchMarketItems - minting, buying and selling
+        //return the number of unsold items
+
+        function fetchMarketTokens() public view returns(MarketToken[] memory) {
+            uint itemCount = _tokenIds.current();
+            uint unsoldItemCount = _tokenIds.current() - _tokensSold.current();
+            uint currentIndex = 0;
+
+            // looping over the number of items created (if number has not been sold populate the array)
+
+            MarketToken[] memory items = new MarketToken[](unsoldItemCount);
+            for(uint i = 0; i < itemCount; i++){
+                if(idToMarketToken[i + 1].owner == address(0)) {
+                    uint currentId = i +1;
+                    MarketToken storage currentItem = idToMarketToken[currentId];
+                    items[currentIndex] = currentItem;
+                    currentIndex += 1;
+
+                }
+            }
+
+            return items;
+
+        }
+
+            //return nfts that the user has purchased
+
+            function fetchMyNFTs() public view returns (MarketToken[] memory) {
+                uint totalItemCount = _tokenIds.current();
+                //a second counter for each individual user
+                uint itemCount = 0;
+                uint currentIndex = 0;
+
+                for(uint i = 0; i < totalItemCount; i++) {
+                    if(idToMarketToken[i + 1].owner == msg.sender) {
+                        itemCount += 1;
+                    }
+                }
+            
+
+            //second loop to loop through the amount you have purchased with itemcount
+            //check to see if the owner address is equal to msg.sender
+
+
+                MarketToken[] memory items = new MarketToken[](itemCount);
+                for(uint i = 0; i < totalItemCount; i++) {
+                    if(idToMarketToken[ i + 1].owner == msg.sender) {
+                        uint currentId = idToMarketToken[i + 1].itemId;
+                        // current array
+                        MarketToken storage currentItem = idToMarketToken[currentId];
+                        items[currentIndex] = currentItem;
+                        currentIndex += 1;
+                    }
+            }
+
+            return items;
+
+  }
 }
