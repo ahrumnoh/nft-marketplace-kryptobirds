77171b9fc44d09a8473c481bb6cb9696afb08b13
Rose Noh <sydneyroseenglish@gmail.com>
2022-07-06 17:54:18 +0900

    updated
    MarketPlace Web3 site

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,39 @@
 import '../styles/globals.css'
+import './app.css'
+import Link from 'next/link'
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+function KryptoBirdMarketPlace({Component, pageProps}) {
+  return (
+    <div>
+      <nav className= 'border-b p-6' style={{backgroundColor: 'purple'}}>
+        <p className = 'text-4x1 font-bold text-white'>KryptoBirdMarketPlace</p>
+        <div className=' flex mt-4 justfy-center'>
+
+          <Link href = '/'>
+            <a className = 'mr-4'>Main MarketPlace
+            </a>
+          </Link>
+
+          <Link href = '/mint-item'>
+            <a className = 'mr-6'>Mint Tokens
+            </a>
+          </Link>
+
+          <Link href = '/my-nfts'>
+            <a className = 'mr-6'>My NFTs
+            </a>
+          </Link>
+
+          <Link href = '/account-dashboard'>
+            <a className = 'mr-6'> Account-Dashboard
+            </a>
+          </Link>
+
+        </div>
+      </nav>
+      <Component {...pageProps} />
+    </div>
+  )
 }
 
-export default MyApp
+export default KryptoBirdMarketPlace
